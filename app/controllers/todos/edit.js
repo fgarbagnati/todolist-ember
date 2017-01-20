f0213cca1369f5e1307d4466d613af1128de4c94
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-19 21:39:24 -0800

    Adds edit functionality

--- app/controllers/todos/edit.js
+++ app/controllers/todos/edit.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	actions: {
+		editTodo: function (id) {
+			var self = this;
+			var title = this.get('model.title');
+			var body = this.get('model.body');
+			var date = this.get('model.date');
+
+			this.store.findRecord('todo', id).then(function (todo) {
+				todo.set('title', title);
+				todo.set('body', body);
+				todo.set('date', new Date(date));
+
+				// Save to FireBase
+				todo.save();
+
+				// Redirect
+				self.transitionToRoute('todos');
+			});
+		}
+	}
 });
